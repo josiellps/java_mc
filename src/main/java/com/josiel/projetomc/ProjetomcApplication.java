afc9544910784bb96a011e8a7a8622ae8fe890d3
Josiel Lopes <lopes1josiel@gmail.com>
2018-04-03 23:31:37 -0300

    Criação do projeto de modelo conceitual.

---
+++ src/main/java/com/josiel/projetomc/ProjetomcApplication.java
@@ -0,0 +1,12 @@
+package com.josiel.projetomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+}

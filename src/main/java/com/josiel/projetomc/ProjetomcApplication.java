05bc36508ee7bd6cdf23ab15d8f7c069ef512a20
Josiel Lopes <lopes1josiel@gmail.com>
2018-04-07 12:18:34 -0300

    Criando operação de instanciação - Categoria

--- src/main/java/com/josiel/projetomc/ProjetomcApplication.java
+++ src/main/java/com/josiel/projetomc/ProjetomcApplication.java
@@ -1,12 +1,32 @@
 package com.josiel.projetomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.josiel.projetomc.domain.Categoria;
+import com.josiel.projetomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class ProjetomcApplication {
+public class ProjetomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }

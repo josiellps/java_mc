d6eb86a197b8c87c0da894215b9f66176e209a73
Josiel Lopes <lopes1josiel@gmail.com>
2018-04-16 21:30:25 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/josiel/projetomc/services/ClienteService.java
+++ src/main/java/com/josiel/projetomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.josiel.projetomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.josiel.projetomc.domain.Cliente;
+import com.josiel.projetomc.dto.ClienteDTO;
 import com.josiel.projetomc.repositories.ClienteRepository;
+import com.josiel.projetomc.services.exceptions.DataIntegrityException;
 
 import javassist.tools.rmi.ObjectNotFoundException;
 
@@ -16,7 +23,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> cat = repo.findById(id);
 		if (cat == null) {
 			try {
@@ -27,5 +34,47 @@ public class ClienteService {
 			}
 		}
 		return cat.orElse(null);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj=find(obj.getId());
+		updateData(newObj,obj);		
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		// TODO Auto-generated method stub
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			// TODO: handle exception
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		@SuppressWarnings("deprecation")
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO obj) {
+		return new Cliente(obj.getId(), obj.getNome(), obj.getEmail(), null, null);
+	}
+
+	public Cliente insert(Cliente obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
